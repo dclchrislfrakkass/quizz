04e3435fd4a0a866dbc661953d99143cf967f332
ChrisL_frakkass <43646777+dclchrislfrakkass@users.noreply.github.com>
2019-01-28 15:32:14 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Quizz</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<header>
+
+
+<!--Navbar-->
+<nav class="navbar navbar-light navbar-1 top-fixed">
+
+<!-- Navbar brand -->
+<a class="navbar-brand" href="#">Quizz</a>
+
+<!-- Collapse button -->
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"
+aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+<!-- Collapsible content -->
+<div class="collapse navbar-collapse" id="navbarSupportedContent15">
+
+<!-- Links -->
+<ul class="navbar-nav mr-auto">
+<li class="nav-item active">
+<a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Connexion</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Contact</a>
+</li>
+</ul>
+<!-- Links -->
+
+</div>
+<!-- Collapsible content -->
+
+</nav>
+</header>
+<body>
+<div class="top">
+<h1>Les mieux notés</h1>
+
+<div class="resultats">
+<br>
+<i class="fas fa-location-arrow"></i>  resultat 1<br>
+<i class="fas fa-location-arrow"></i>  resultat 2<br>
+<i class="fas fa-location-arrow"></i>  resultat 3<br>
+<i class="fas fa-location-arrow"></i>  resultat 4<br>
+<i class="fas fa-location-arrow"></i>  resultat 5<br>
+<i class="fas fa-location-arrow"></i>  resultat 6<br>
+    <br>
+</div>
+</div>
+
+
+<div class="genres">
+
+<div class="container">
+
+<div class="row">
+<div class="col-7 genre">GENRE</div>
+<div class="col-4 genre">GENRE</div>
+</div>
+
+<div class="row">
+<div class="col-4 genre">GENRE</div>
+<div class="col-7 genre">GENRE</div>
+</div>
+
+
+<div class="row">
+<div class="col-7 genre">GENRE</div>
+<div class="col-4 genre">GENRE</div>
+</div>
+
+<div class="row">
+    <div class="col-4 genre">GENRE</div>
+    <div class="col-7 genre">GENRE</div>
+    </div>
+</div>
+
+</div>
+
+<footer>
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2018 Copyright:
+<a href="http://lacroix-c.fr" target="blank"> Chris L</a>
+</div>
+<!-- Copyright -->
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+</html>
